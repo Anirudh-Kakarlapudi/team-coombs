4a8ffcd75488f24fa20a53528b5c6c90cca4aa3a
Anant Tripathi <tripathianant12@gmail.com>
2019-04-02 21:26:58 -0400

    edited the main script

--- main_script.py
+++ main_script.py
@@ -1,21 +1,13 @@
-import os
-import argparse
-from src.unet import UNet
+# import argparse
 from src.nmf import model_implementaiton
 
 # creating the parser
-parser = argparse.ArgumentParser(description='Trains the model and outputs predictions.')
 
-#adding agrument to the parser
-parser.add_argument('--model', type=str, choices=['unet','nmf'], 
-    default='unet', help = 'model for neuron finding')
+# parser = argparse.ArgumentParser(description='Trains the model 
+# and outputs predictions.')
 
-#choose the model
-args = parser.parse_args()
-model = args.model
+# choose the model
+# args = parser.parse_args()
 
-if model == 'unet':
-    UNet(model)
-elif model == 'nmf':
-    model_implementaiton()
+model_implementaiton()
 
